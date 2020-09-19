15291ef0c62e4bfab40b7cbbda65b698dd48b67a
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-19 16:31:34 +0100

    Removed Blur

--- assets/main.js
+++ assets/main.js
@@ -11,12 +11,6 @@ window.addEventListener("load", () => {
   });
 });
 
-$(document).ready(function () {
-  $(".hero-image-preload")
-    .removeClass("hero-image-preload")
-    .addClass("hero-image");
-});
-
 function decodeEmail(encodedString) {
   // Holds the final output
   var email = "";
