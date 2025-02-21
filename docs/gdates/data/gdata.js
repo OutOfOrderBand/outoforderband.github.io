99f099d60694755c5e0650633e697c3ea1974a29
NormanSolutions <hi@normansolutions.co.uk>
2025-02-21 18:20:04 +0000

    Updated GDates

--- docs/gdates/data/gdata.js
+++ docs/gdates/data/gdata.js
@@ -1,184 +1,184 @@
 var gData = {
-  "GDates": [
+  GDates: [
     {
-      "Id": "6f3fd3a1-66ae-406c-bfe1-ed94bd65e644",
-      "Date": "Sun 23 Feb 2025 (23/02/2025)",
-      "Description": "Already Gig Booked"
+      Id: "6f3fd3a1-66ae-406c-bfe1-ed94bd65e644",
+      Date: "Sun 23 Feb 2025 (23/02/2025)",
+      Description: "Already Gig Booked",
     },
     {
-      "Id": "2b3ba2ca-0264-4d3b-8437-360bf726b6cd",
-      "Date": "Fri 25 Apr 2025 (25/04/2025)",
-      "Description": "Already Gig Booked"
+      Id: "2b3ba2ca-0264-4d3b-8437-360bf726b6cd",
+      Date: "Fri 25 Apr 2025 (25/04/2025)",
+      Description: "Already Gig Booked",
     },
     {
-      "Id": "c19ad63f-4520-48be-a98c-0241bfe99332",
-      "Date": "Sat 10 May 2025 (10/05/2025)",
-      "Description": "Already Gig Booked"
+      Id: "c19ad63f-4520-48be-a98c-0241bfe99332",
+      Date: "Sat 10 May 2025 (10/05/2025)",
+      Description: "Already Gig Booked",
     },
     {
-      "Id": "940e75eb-72e9-4272-854f-2434f50bb987",
-      "Date": "Fri 11 Jul 2025 (11/07/2025)",
-      "Description": "Already Gig Booked"
+      Id: "940e75eb-72e9-4272-854f-2434f50bb987",
+      Date: "Fri 11 Jul 2025 (11/07/2025)",
+      Description: "Already Gig Booked",
     },
     {
-      "Id": "dfa7e211-d2b2-441f-a116-418726428e3d",
-      "Date": "Sat 26 Jul 2025 (26/07/2025)",
-      "Description": "Already Gig Booked"
+      Id: "dfa7e211-d2b2-441f-a116-418726428e3d",
+      Date: "Sat 26 Jul 2025 (26/07/2025)",
+      Description: "Already Gig Booked",
     },
     {
-      "Id": "4565726e-8d17-4814-88b7-47a9e545b239",
-      "Date": "Sat 06 Sep 2025 (06/09/2025)",
-      "Description": "Already Gig Booked"
+      Id: "4565726e-8d17-4814-88b7-47a9e545b239",
+      Date: "Sat 06 Sep 2025 (06/09/2025)",
+      Description: "Already Gig Booked",
     },
     {
-      "Id": "e141b621-d9dc-4a17-9a19-66343e417a6e",
-      "Date": "Sat 22 Nov 2025 (22/11/2025)",
-      "Description": "Already Gig Booked"
+      Id: "e141b621-d9dc-4a17-9a19-66343e417a6e",
+      Date: "Sat 22 Nov 2025 (22/11/2025)",
+      Description: "Already Gig Booked",
     },
     {
-      "Id": "ec651539-763d-4d61-8364-bc079661c762",
-      "Date": "Sat 06 Dec 2025 (06/12/2025)",
-      "Description": "Already Gig Booked"
+      Id: "ec651539-763d-4d61-8364-bc079661c762",
+      Date: "Sat 06 Dec 2025 (06/12/2025)",
+      Description: "Already Gig Booked",
     },
     {
-      "Id": "7eaa722f-16fc-47eb-8569-e5b699be39c6",
-      "Date": "Fri 14 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "7eaa722f-16fc-47eb-8569-e5b699be39c6",
+      Date: "Fri 14 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "f02ed46c-f022-4069-9ba7-fc5ce07c553a",
-      "Date": "Sat 15 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "f02ed46c-f022-4069-9ba7-fc5ce07c553a",
+      Date: "Sat 15 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "51626347-a2ec-459e-a073-c0d5f450410b",
-      "Date": "Sun 16 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "51626347-a2ec-459e-a073-c0d5f450410b",
+      Date: "Sun 16 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "959b9042-c2a4-4990-888c-6d262195d4c6",
-      "Date": "Mon 17 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "959b9042-c2a4-4990-888c-6d262195d4c6",
+      Date: "Mon 17 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "ce3c025f-a3f0-4132-8c9f-eaf7f37b3e14",
-      "Date": "Tue 18 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "ce3c025f-a3f0-4132-8c9f-eaf7f37b3e14",
+      Date: "Tue 18 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "1ba1a1f1-1cf6-4b99-ac26-65416f11c334",
-      "Date": "Wed 19 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "1ba1a1f1-1cf6-4b99-ac26-65416f11c334",
+      Date: "Wed 19 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "16922fd2-5851-4d7c-ac48-79a6c282e958",
-      "Date": "Thu 20 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "16922fd2-5851-4d7c-ac48-79a6c282e958",
+      Date: "Thu 20 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "c7760a9c-4012-431e-b86b-764f9bcfc634",
-      "Date": "Fri 21 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "c7760a9c-4012-431e-b86b-764f9bcfc634",
+      Date: "Fri 21 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "4ddaeac2-dd07-4121-bed4-d42aae5dc402",
-      "Date": "Sat 22 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "4ddaeac2-dd07-4121-bed4-d42aae5dc402",
+      Date: "Sat 22 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "8a076cbe-7368-41f8-84e4-a4a82a964a7f",
-      "Date": "Sun 23 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "8a076cbe-7368-41f8-84e4-a4a82a964a7f",
+      Date: "Sun 23 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "0dccffda-1ef7-4874-aa17-d70c2aafdd38",
-      "Date": "Mon 24 Mar 2025",
-      "Description": "Clive Unavailable"
+      Id: "0dccffda-1ef7-4874-aa17-d70c2aafdd38",
+      Date: "Mon 24 Mar 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "5246d2e9-2b8f-42a3-81ea-4bbe0d99a4ef",
-      "Date": "Fri 04 Apr 2025",
-      "Description": "Clive Unavailable"
+      Id: "5246d2e9-2b8f-42a3-81ea-4bbe0d99a4ef",
+      Date: "Fri 04 Apr 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "54c2fb54-1c5c-4204-8bcb-56d212da61f5",
-      "Date": "Sat 05 Apr 2025",
-      "Description": "Clive Unavailable"
+      Id: "54c2fb54-1c5c-4204-8bcb-56d212da61f5",
+      Date: "Sat 05 Apr 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "725b4b82-51c2-41ba-9748-d95d5e668883",
-      "Date": "Sun 06 Apr 2025",
-      "Description": "Clive Unavailable"
+      Id: "725b4b82-51c2-41ba-9748-d95d5e668883",
+      Date: "Sun 06 Apr 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "315a4dc5-30a1-4852-899b-ccd8e6d754fc",
-      "Date": "Mon 09 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "315a4dc5-30a1-4852-899b-ccd8e6d754fc",
+      Date: "Mon 09 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "d57ab191-4791-4821-8768-2d7c6365de7d",
-      "Date": "Tue 10 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "d57ab191-4791-4821-8768-2d7c6365de7d",
+      Date: "Tue 10 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "ba3d66ec-4278-4525-9e10-b454c4f0ea04",
-      "Date": "Wed 11 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "ba3d66ec-4278-4525-9e10-b454c4f0ea04",
+      Date: "Wed 11 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "4b7b55ec-3a77-4459-9c9c-4ef78528858a",
-      "Date": "Thu 12 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "4b7b55ec-3a77-4459-9c9c-4ef78528858a",
+      Date: "Thu 12 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "4de4d118-485b-461e-875a-ab5445cadab6",
-      "Date": "Fri 13 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "4de4d118-485b-461e-875a-ab5445cadab6",
+      Date: "Fri 13 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "77fcf910-6154-4784-9ca3-58abb80dc326",
-      "Date": "Sat 14 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "77fcf910-6154-4784-9ca3-58abb80dc326",
+      Date: "Sat 14 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "f8c23bfd-452c-4abd-ab0b-9eca22e2c4ff",
-      "Date": "Sun 15 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "f8c23bfd-452c-4abd-ab0b-9eca22e2c4ff",
+      Date: "Sun 15 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "eaf80e83-5cc0-4923-925e-f5c2c53f9f51",
-      "Date": "Mon 16 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "eaf80e83-5cc0-4923-925e-f5c2c53f9f51",
+      Date: "Mon 16 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "ca67383c-bcca-4420-bf50-741bee5329f7",
-      "Date": "Tue 17 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "ca67383c-bcca-4420-bf50-741bee5329f7",
+      Date: "Tue 17 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "5d9d617d-3e8f-4b47-b3ef-9deccf67576d",
-      "Date": "Wed 18 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "5d9d617d-3e8f-4b47-b3ef-9deccf67576d",
+      Date: "Wed 18 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "6ea5c514-44cc-4cff-b9d4-a5f93efdc948",
-      "Date": "Thu 19 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "6ea5c514-44cc-4cff-b9d4-a5f93efdc948",
+      Date: "Thu 19 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "fa6fe709-7f51-452a-96dd-6bef4bf9bc91",
-      "Date": "Fri 20 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "fa6fe709-7f51-452a-96dd-6bef4bf9bc91",
+      Date: "Fri 20 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "3cf3130e-9063-4efb-a103-894bfcd4f9bb",
-      "Date": "Sat 21 Jun 2025",
-      "Description": "Clive Unavailable"
+      Id: "3cf3130e-9063-4efb-a103-894bfcd4f9bb",
+      Date: "Sat 21 Jun 2025",
+      Description: "Clive Unavailable",
     },
     {
-      "Id": "ef3bb106-8cd7-4b0f-80c7-541b9048d998",
-      "Date": "Sun 22 Jun 2025",
-      "Description": "Clive Unavailable"
-    }
-  ]
-}
+      Id: "ef3bb106-8cd7-4b0f-80c7-541b9048d998",
+      Date: "Sun 22 Jun 2025",
+      Description: "Clive Unavailable",
+    },
+  ],
+};
