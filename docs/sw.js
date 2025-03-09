19b6eb19a55e2f3da1fd4694d46749d3961f6daa
NormanSolutions <hi@normansolutions.co.uk>
2025-03-09 20:12:03 +0000

    Updated Gig

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Update 'version' if you need to refresh the cache
-  var version = "NS24022025V6::CacheFirstSafe";
+  var version = "NS09032025V1::CacheFirstSafe";
   var offlineUrl = "/offline.html";
 
   // Store core files in a cache (including a page to display when offline)
