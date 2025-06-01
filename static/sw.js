5a41e618b3e72f916cf48d5726ba3f16a5f8af9b
NormanSolutions <hi@normansolutions.co.uk>
2025-06-01 22:31:10 +0100

    Update Improved Website

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "NS01062025V2::CacheFirstSafe";
+  const version = "NS01062025V3::CacheFirstSafe";
   const offlineUrl = "/offline.html";
 
   async function updateStaticCache() {
