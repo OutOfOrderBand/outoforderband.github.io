3f116bb140cd218f3cce619bac2162232c5bc83d
NormanSolutions <hi@normansolutions.co.uk>
2025-09-09 16:07:31 +0100

    Update Gig

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "NS09092025V1::CacheFirstSafe";
+  const version = "NS09092025V3::CacheFirstSafe";
   const offlineUrl = "/offline.html";
 
   async function updateStaticCache() {
