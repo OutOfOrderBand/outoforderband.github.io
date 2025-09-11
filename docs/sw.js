89cfc6248fd9b90b4aa7ee439c9ddb65cf410647
NormanSolutions <hi@normansolutions.co.uk>
2025-09-11 18:19:49 +0100

    Update Dates

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "NS11092025V8::CacheFirstSafe";
+  const version = "NS11092025V9::CacheFirstSafe";
   const offlineUrl = "/offline.html";
 
   async function updateStaticCache() {
