88f5362ab8f5c73010f8b3f0aabdecafe2021b03
NormanSolutions <hi@normansolutions.co.uk>
2025-11-03 15:24:24 +0000

    Updated Dates

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "NSV007::CacheFirstSafe";
+  const version = "NSV008::CacheFirstSafe";
   const offlineUrl = "/offline.html";
   const offlineImage = "/offline.png"; // <-- add this to your precache
 
