2c97cc8be5e8e4da3c8eafc159ed628f1bda1c8f
NormanSolutions <hi@normansolutions.co.uk>
2026-02-21 17:37:51 +0000

    Update Metronome

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "NSV041::CacheFirstSafe";
+  const version = "NSV042::CacheFirstSafe";
   const offlineUrl = "/offline.html";
   const offlineImage = "/offline.png"; // <-- add this to your precache
 
