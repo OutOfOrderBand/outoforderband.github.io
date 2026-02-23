38c899749a2b9ef661b3370d1598f1a2f5d060c6
NormanSolutions <hi@normansolutions.co.uk>
2026-02-23 08:45:37 +0000

    Update SetList

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "NSV042::CacheFirstSafe";
+  const version = "NSV043::CacheFirstSafe";
   const offlineUrl = "/offline.html";
   const offlineImage = "/offline.png"; // <-- add this to your precache
 
