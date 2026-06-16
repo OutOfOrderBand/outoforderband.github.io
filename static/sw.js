735e800baa34276ed647763af8c70bb5fcad7cef
NormanSolutions <hi@normansolutions.co.uk>
2026-06-16 20:56:41 +0100

    Update Gig

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "NSV062::CacheFirstSafe";
+  const version = "NSV063::CacheFirstSafe";
   const offlineUrl = "/offline.html";
   const offlineImage = "/offline.png"; // <-- add this to your precache
 
