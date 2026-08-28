448b4f02cce79aba1ce46b0a2a3b8ef140599051
NormanSolutions <hi@normansolutions.co.uk>
2026-08-28 09:48:13 +0100

    Update Duo

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "NSV075::CacheFirstSafe";
+  const version = "NSV077::CacheFirstSafe";
   const offlineUrl = "/offline.html";
   const offlineImage = "/offline.png"; // <-- add this to your precache
 
